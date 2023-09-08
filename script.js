5e3a336d55096e538892dd02a98d8e77cd83023d
Gaurav <4gaurav07@gmail.com>
2023-09-08 19:38:37 +0530

    Calcy

---
+++ script.js
@@ -0,0 +1,21 @@
+
+document / addEventListener('DOMContentLoaded', () => {
+
+    const display = document.querySelector('#display');
+    const buttons = document.querySelectorAll('button');
+
+    buttons.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            if (btn.id == '=') {
+                display.value = eval(display.value);
+            } else if (btn.id === 'C') {
+                display.value = "";
+            } else if (btn.id == "back") {
+                display.value = display.value.slice(0, -1);
+            } else {
+                display.value += btn.id;
+            }
+        });
+    });
+})
+
